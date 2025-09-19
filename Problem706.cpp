f62d3baa1621806733e771c1be30039a814e8fc0
HarshKorde5 <harshkorde05@gmail.com>
2025-09-19 22:40:41 +0530

    problem 706

---
+++ Problem706.cpp
@@ -0,0 +1,79 @@
+/*
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+public:
+    int key, val;
+    Node* next;
+    Node(int k, int v, Node* n) {
+        key = k;
+        val = v;
+        next = n;
+    }
+};
+class MyHashMap {
+public:
+    const static int size = 19997;
+    Node* data[size] = {};
+    int hash(int key) {
+        return key % size;
+    }
+    void put(int key, int val) {
+        remove(key);
+        int h = hash(key);
+        Node* node = new Node(key, val, data[h]);
+        data[h] = node;
+    }    
+    int get(int key) {
+        int h = hash(key);
+        Node* node = data[h];
+        for (; node != NULL; node = node->next)
+            if (node->key == key) return node->val;
+        return -1;
+    }    
+    void remove(int key) {
+        int h = hash(key);
+        Node* node = data[h];
+        if (node == NULL) return;
+        if (node->key == key) {
+            data[h] = node->next;
+            delete node;
+        } else for (; node->next != NULL; node = node->next)
+            if (node->next->key == key) {
+                Node* temp = node->next;
+                node->next = temp->next;
+                delete temp;
+                return;
+            }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+
+ int main(){
+    MyHashMap* obj = new MyHashMap();
+    obj->put(10, 101);
+    int param_2 = obj->get(10);
+    obj->remove(10);
+
+    return 0;
+ }
