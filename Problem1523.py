8a2e6908906f549dd3af0665e702a57ad715e733
HarshKorde5 <harshkorde05@gmail.com>
2025-12-07 11:01:07 +0530

    problem 1523

---
+++ Problem1523.py
@@ -0,0 +1,5 @@
+# Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+low = 3
+high = 11
+print(f"Total odd numbers in the range {low} => {high} are : {((high + 1) // 2) - (low // 2)}")
