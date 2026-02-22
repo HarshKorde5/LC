55e14c191a6a2a84d6a5198390290f6b36ba795b
HarshKorde5 <harshkorde05@gmail.com>
2026-02-22 19:52:50 +0530

    problem 868

---
+++ Problem868.java
@@ -0,0 +1,40 @@
+/*
+Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
+
+Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+
+ 
+*/
+
+class Solution {
+    public int binaryGap_1(int N) {
+        int last = -1, ans = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((N >> i) & 1) > 0) {
+                if (last >= 0)
+                    ans = Math.max(ans, i - last);
+                last = i;
+            }
+
+        return ans;
+    }
+    public int binaryGap(int N) {
+        int res = 0;
+        for (int d = -32; N > 0; N /= 2, d++)
+            if (N % 2 == 1) {
+                res = Math.max(res, d);
+                d = 0;
+            }
+        return res;
+    }
+}
+
+class Problem868{
+    public static void main(String[] args) {
+        Solution sobj = new Solution();
+
+        int ret = sobj.binaryGap(22);
+
+        System.out.println(ret);
+    }
+}
