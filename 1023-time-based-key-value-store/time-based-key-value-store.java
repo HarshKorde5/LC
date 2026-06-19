9cee05da81146ebec3f98a4e71029434c446d4bd
Harsh Munjaji Korde <141399156+HarshKorde5@users.noreply.github.com>
2026-06-19 11:43:38 +0530

    Time: 138 ms (56.25%) | Memory: 108.3 MB (62.98%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,7 +1,7 @@
-class TimeMap {
+class TimeMap_01 {
     Map<String, TreeMap<Integer, String>> map;
 
-    public TimeMap() {
+    public TimeMap_01() {
         map = new HashMap<>();
     }
     
@@ -17,5 +17,38 @@ class TimeMap {
         Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
 
         return entry == null ? "" : entry.getValue();
+    }
+}
+
+class TimeMap {
+
+    Map<String, List<Pair<Integer,String>>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new ArrayList<>()).add(new Pair<>(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        List<Pair<Integer,String>> values = map.getOrDefault(key, new ArrayList<>());
+
+        int left = 0, right = values.size() - 1;
+        String result = "";
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            if(values.get(mid).getKey() <= timestamp){
+                result = values.get(mid).getValue();
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        return result;
     }
 }
