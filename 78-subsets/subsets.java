bfd5ea876f87a63294f471205d634ca611f09425
Harsh Munjaji Korde <141399156+HarshKorde5@users.noreply.github.com>
2026-07-20 20:27:16 +0530

    Time: 1 ms (86.76%) | Memory: 44 MB (88.74%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,36 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        for (int num : nums) {
+            int size = result.size();
+
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
+        }
+
+        return result;
+    }
+
+    public List<List<Integer>> subsets_rec(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
         List<Integer> currsubset = new ArrayList<>();
 
-        rec(nums,0,currsubset,result);
+        rec(nums, 0, currsubset, result);
         return result;
     }
 
-    private void rec(int[] nums, int idx, List<Integer> currsubset, List<List<Integer>> result){
-        if(idx >= nums.length){
+    private void rec(int[] nums, int idx, List<Integer> currsubset, List<List<Integer>> result) {
+        if (idx >= nums.length) {
             result.add(new ArrayList<>(currsubset));
             return;
         }
-
 
         currsubset.add(nums[idx]);
         rec(nums, idx + 1, currsubset, result);
