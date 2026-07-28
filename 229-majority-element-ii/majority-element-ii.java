1eadfcaa80eb787dac456714767d648c976b8089
Harsh Munjaji Korde <141399156+HarshKorde5@users.noreply.github.com>
2026-07-28 21:11:14 +0530

    Time: 10 ms (47.48%) | Memory: 50.9 MB (98.77%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -13,7 +13,7 @@ public class Solution {
         return new ArrayList<>(res);
     }
 
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement_counting(int[] nums) {
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) {
             count.put(num, count.getOrDefault(num, 0) + 1);
@@ -22,6 +22,37 @@ public class Solution {
         List<Integer> res = new ArrayList<>();
         for (int key : count.keySet()) {
             if (count.get(key) > nums.length / 3) {
+                res.add(key);
+            }
+        }
+
+        return res;
+    }
+    
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+
+            if (count.size() > 2) {
+                Map<Integer, Integer> newCount = new HashMap<>();
+                for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+                    if (entry.getValue() > 1) {
+                        newCount.put(entry.getKey(), entry.getValue() - 1);
+                    }
+                }
+                count = newCount;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : count.keySet()) {
+            int frequency = 0;
+            for (int num : nums) {
+                if (num == key) frequency++;
+            }
+            if (frequency > nums.length / 3) {
                 res.add(key);
             }
         }
