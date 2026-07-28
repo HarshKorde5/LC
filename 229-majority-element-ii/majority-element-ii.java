4c8197bec5f5ca42dac608d826e61d84d92b4fc3
Harsh Munjaji Korde <141399156+HarshKorde5@users.noreply.github.com>
2026-07-28 21:09:11 +0530

    Time: 14 ms (41.59%) | Memory: 53.4 MB (7.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement_brute(int[] nums) {
         Set<Integer> res = new HashSet<>();
         for (int num : nums) {
             int count = 0;
@@ -11,5 +11,21 @@ public class Solution {
             }
         }
         return new ArrayList<>(res);
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : count.keySet()) {
+            if (count.get(key) > nums.length / 3) {
+                res.add(key);
+            }
+        }
+
+        return res;
     }
 }
