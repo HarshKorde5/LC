54145434a9a5df0cff465b903c9ef8bd67ae9337
Harsh Munjaji Korde <141399156+HarshKorde5@users.noreply.github.com>
2026-08-25 20:41:59 +0530

    Time: 8 ms (15.06%) | Memory: 47.2 MB (68.54%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,19 +1,55 @@
 class Solution {
+    private int[][] grid;
+    private boolean[][] visited;
+    private int rows, cols;
+
     public int islandPerimeter(int[][] grid) {
+        this.grid = grid;
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        this.visited = new boolean[rows][cols];
+
+        //just to start from a cell which is guarrented to be connected
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(i, j);
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private int dfs(int i, int j) {
+        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == 0) {
+            return 1;
+        }
+
+        if (visited[i][j]) {
+            return 0;
+        }
+
+        visited[i][j] = true;
+
+        return dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1);
+    }
+
+    public int islandPerimeter_01(int[][] grid) {
         int m = grid.length, n = grid[0].length, res = 0;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
 
                     //top
-                    res += (i - 1 < 0 || grid[i-1][j] == 0) ? 1 : 0;                    
+                    res += (i - 1 < 0 || grid[i - 1][j] == 0) ? 1 : 0;
                     //right
                     res += (j + 1 >= n || grid[i][j + 1] == 0) ? 1 : 0;
                     //bottom
                     res += (i + 1 >= m || grid[i + 1][j] == 0) ? 1 : 0;
                     //left
-                    res += (j - 1 < 0 || grid[i][j-1] == 0) ? 1 : 0;
+                    res += (j - 1 < 0 || grid[i][j - 1] == 0) ? 1 : 0;
                 }
             }
         }
