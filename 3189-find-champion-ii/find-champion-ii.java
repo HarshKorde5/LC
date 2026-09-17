22c17a22550534aae0dfd43bb31476cff6c02d68
Harsh Munjaji Korde <141399156+HarshKorde5@users.noreply.github.com>
2026-09-17 16:29:01 +0530

    Time: 1 ms (100.00%) | Memory: 48.1 MB (36.73%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -1,18 +1,38 @@
 class Solution {
-    public int findChampion(int n, int[][] edges) {
+    public int findChampion_01(int n, int[][] edges) {
         Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             set.add(i);
         }
 
-        for(int[] e : edges){
+        for (int[] e : edges) {
             set.remove(e[1]);
         }
 
-        if(set.size() == 1){
+        if (set.size() == 1) {
             return set.iterator().next();
         }
 
         return -1;
+    }
+
+    public int findChampion(int n, int[][] edges) {
+        int[] indegree = new int[n];
+
+        for (int[] edge : edges) {
+            indegree[edge[1]]++;
+        }
+
+        int champ = -1;
+        int champCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                champCount++;
+                champ = i;
+            }
+        }
+
+        return champCount > 1 ? -1 : champ;
     }
 }
